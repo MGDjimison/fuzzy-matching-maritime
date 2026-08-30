b063dc285f76b1219ce2c75a9ab816c538c92204
djimi <djimi9530@gmail.com>
2026-08-30 17:33:02 +0200

    potential matches

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import polars as pl
-from src.utils import clean_name
+from src.utils import clean_name, get_similar_names
 
 if __name__ == "__main__":
-    # df = pl.read_csv("data/mock_maritime_companies.csv")
-    # print(df.head(5))
-    print(clean_name("Company 123!@# Name"))  # Example usage of the clean_name function
+    df = pl.read_csv("data/mock_maritime_companies.csv")
+    matches_df = get_similar_names(df)
+    print(matches_df.head(10))
+                
+    
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import polars as pl
+from thefuzz import fuzz, process
 
 def clean_name(name: str) -> str:
     """
@@ -14,3 +15,23 @@ def clean_name(name: str) -> str:
     cleaned_name = ''.join([char for char in name if not char.isdigit() and char.isalpha()])
 
     return cleaned_name.upper()
+
+
+def get_similar_names(df: pl.DataFrame) -> pl.DataFrame:
+    # all active company names
+    active_companies_df = df.filter(pl.col("is_active") == True)
+    similar_companies = []
+
+    for company_name in df["name"].to_list():
+        for active_company in active_companies_df["name"].to_list():
+            # fuzzy matching score between the two names
+            matching = fuzz.token_sort_ratio(company_name, active_company)
+            if matching >= 85:
+                similar_companies.append((company_name, active_company, matching))
+
+    similar_companies_df = pl.DataFrame(data=similar_companies, schema=["company_name", "similar_company", "fuzzy_matching_score"])
+
+    # do not include exact matches (fuzzy_matching_score == 100)
+    similar_companies_df = similar_companies_df.remove(pl.col("fuzzy_matching_score") == 100)
+
+    return similar_companies_df
