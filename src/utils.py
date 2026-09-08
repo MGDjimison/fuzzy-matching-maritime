18c1f243babb6c5233aee81515672e43b368409c
djimi <djimi9530@gmail.com>
2026-09-08 10:38:36 +0200

    remove old function

--- src/utils.py
+++ src/utils.py
@@ -25,25 +25,6 @@ def clean_name(name: str) -> str:
     return name.upper()
 
 
-def get_similar_names(df: pl.DataFrame) -> pl.DataFrame:
-    # all active company names
-    active_companies_df = df.filter(pl.col("is_active") == True)
-    similar_companies = []
-
-    for company_name in df["name"].to_list():
-        for active_company in active_companies_df["name"].to_list():
-            # fuzzy matching score between the two names
-            matching = fuzz.token_sort_ratio(company_name, active_company)
-            if matching >= 85:
-                similar_companies.append((company_name, active_company, matching))
-
-    similar_companies_df = pl.DataFrame(data=similar_companies, schema=["company_name", "similar_company", "fuzzy_matching_score"])
-
-    # do not include exact matches (fuzzy_matching_score == 100)
-    similar_companies_df = similar_companies_df.remove(pl.col("fuzzy_matching_score") == 100)
-
-    return similar_companies_df
-
 
 def get_top2_similar_companies(company: str, df: pl.DataFrame) -> pl.DataFrame:
     if company not in df["name"].to_list():
